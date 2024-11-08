870422b90fa98f2a03b98ac30d284cc796421fc7
komolika05 <komolikakakrani@gmail.com>
2024-11-08 14:41:57 +0530

    basic container, text styles and button styles

--- my_first_project/lib/main.dart
+++ my_first_project/lib/main.dart
@@ -4,9 +4,10 @@ import 'package:my_first_project/ex2.dart';
 void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
-    home: Example1(),
+    home: Firstapp(),
   ));
 }
+
 class Firstapp extends StatelessWidget {
   const Firstapp({super.key});
 
@@ -21,13 +22,34 @@ class Firstapp extends StatelessWidget {
         title: Text("my first app"),
         actions: [Icon(Icons.info)],
       ),
-      body: Container(
-        alignment: Alignment.center,
-        
-        child: Text("Hello world"),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              alignment: Alignment.center,
+              child: Text(
+                "Hello world!",
+                style: TextStyle(fontSize: 24, color: Colors.white, fontWeight: FontWeight.w600),
+              ),
+              width: 200,
+              height: 200,
+              color: Colors.blue,
+            ),
+            SizedBox(height: 20), // Add top margin here
+            ElevatedButton(
+              onPressed: () {
+                print('button pressed');
+              },
+              child: Text('Lets Go..'),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.teal,
+                foregroundColor: Colors.white
+              ),
+            ),
+          ],
+        ),
       ),
-
     );
   }
 }
-
